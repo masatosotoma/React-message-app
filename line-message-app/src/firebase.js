0f6200ca205b10245dd7e7bd4304f5f98bd3e24d
masatosotoma <masatosotoma@gmail.com>
2024-07-07 21:42:42 -0400

    Setting up firebase auth

--- line-message-app/src/firebase.js
+++ line-message-app/src/firebase.js
@@ -1,5 +1,16 @@
 import firebase from "firebase/compat/app";
 
-const firebaseApp = firebase.initializeApp(){
-    
-}
+const firebaseApp = firebase.initializeApp({
+  apiKey: "AIzaSyCsu0_igUj9VwcPBE4f5tvQzq8qsJQOicw",
+  authDomain: "line-clone-43fca.firebaseapp.com",
+  projectId: "line-clone-43fca",
+  storageBucket: "line-clone-43fca.appspot.com",
+  messagingSenderId: "83016784063",
+  appId: "1:83016784063:web:90f15d26748744daf10e0a",
+});
+
+const db = firebaseApp.firestore();
+
+const auth = firebase.auth();
+
+export { db, auth };
