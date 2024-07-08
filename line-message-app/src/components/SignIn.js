c29dd8faa8863da98c3ec0f63078014b87cf6fe0
masatosotoma <masatosotoma@gmail.com>
2024-07-07 21:46:09 -0400

    Setting up the signIn func

--- line-message-app/src/components/SignIn.js
+++ line-message-app/src/components/SignIn.js
@@ -1,9 +1,12 @@
 import { Button } from "@mui/material";
 import React from "react";
+import firebase from "firebase/compat/app";
+import { auth } from "../firebase.js";
 
 function SignIn() {
   function singInWithGoogle() {
-    
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
   }
   return <Button onClick={singInWithGoogle}>Login with Google account</Button>;
 }
