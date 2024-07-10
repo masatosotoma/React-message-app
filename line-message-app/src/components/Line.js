8ec5efc215ac667f0f5fcf0414d495c956a49c3d
masatosotoma <masatosotoma@gmail.com>
2024-07-09 23:36:21 -0400

    Created Line component

---
+++ line-message-app/src/components/Line.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect, useRef } from "react";
+import { db, auth } from "../firebase.js";
+import SendMessage from "./SendMessage.js";
+import SignOut from "./SignOut";
+
+function Line() {
+    return( <div>Line</div>)
+}
+
+export default Line;
