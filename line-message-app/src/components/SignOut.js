28dbaac2e79558175db19415d58e752b6f692df5
masatosotoma <masatosotoma@gmail.com>
2024-07-16 22:19:49 -0400

    Styling Signout button

--- line-message-app/src/components/SignOut.js
+++ line-message-app/src/components/SignOut.js
@@ -1,11 +1,19 @@
 import { Button } from "@mui/material";
 import React from "react";
-import {auth} from "../firebase.js";
+import { auth } from "../firebase.js";
+import CallIcon from "@mui/icons-material/Call";
 
 function SignOut() {
   return (
-    <div>
-      <Button onClick={() => auth.signOut()}>Sign Out</Button>
+    <div className="header">
+      <Button
+        style={{ color: "white", fontSize: "15px" }}
+        onClick={() => auth.signOut()}
+      >
+        Sign Out
+      </Button>
+      <h3>{auth.currentUser.displayName}</h3>
+      <CallIcon />
     </div>
   );
 }
